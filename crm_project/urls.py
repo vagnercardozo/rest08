622cb4beb9caeab3745c83d19ffd75e32fbc20c8
lip2d <>
2022-10-20 10:33:41 -0300

    feat

---
+++ crm_project/urls.py
@@ -0,0 +1,7 @@
+# crm_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('customer/', include('customer.urls')),
+]
